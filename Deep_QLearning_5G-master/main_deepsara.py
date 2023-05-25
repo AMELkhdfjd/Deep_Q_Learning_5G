baa7f294581ea19b15d54d1ce8e71e55bb605547
AMELkhdfjd <ia_khamoum@esi.dz>
2023-05-26 00:23:32 +0200

    fixxxxxxing the problem with the type of nodes being changed after cpying the graph from the author code, ATTTTTTTT

--- Deep_QLearning_5G-master/main_deepsara.py
+++ Deep_QLearning_5G-master/main_deepsara.py
@@ -920,7 +920,7 @@ def main():
                 # controller.substrate = copy.deepcopy(substrate_graphs.get_graph("abilene")) #get substrate    
                 centralized_initial = controller.substrate.graph["centralized_cpu"]
                 bw_initial = controller.substrate.graph["bw"]
-                controller.simulation.set_run_till(1.5)   ## set the run_till variable of SIm to 15, the end of the simulatin is after 15 time units
+                controller.simulation.set_run_till(15)   ## set the run_till variable of SIm to 15, the end of the simulatin is after 15 time units
                                                         ## initially was 15
                 prepare_sim(controller.simulation)   ## creates the arrival events and the twindow_end event to prepare the environment          
                 controller.run()    ## runs all the events of the list one by one, here we execute the run of the class SIm, and a function for each event     
