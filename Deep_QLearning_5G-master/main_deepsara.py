9358f8a67474162faee47a9a63cedf8905d3800e
AMELkhdfjd <ia_khamoum@esi.dz>
2023-08-19 16:53:11 +0200

    removing the reability from the state to test if we need to add the latency there also

--- Deep_QLearning_5G-master/main_deepsara.py
+++ Deep_QLearning_5G-master/main_deepsara.py
@@ -460,7 +460,7 @@ def get_state(substrate,simulation, index): ## returns the state of 9 parmas
     #labels_1 = [[node["p"], node["cpu"]] for node in substrate.graph["nodes"]]
     # Latency solution
     #labels_1 = [[node["p"], node["cpu"], node["l"]] for node in substrate.graph["nodes"]]
-    labels_1 = [[node["p"], node["cpu"], node["l"]] for node in substrate.graph["nodes"]] ## to test if the latency need to be added here
+    labels_1 = [[node["cpu"], node["l"]] for node in substrate.graph["nodes"]] ## to test if the latency need to be added here
 
 
 # Extract links' "source" and "target" values into "graph_1"
